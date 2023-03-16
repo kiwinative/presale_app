e55f903f6583b087d8ea328366dbffb0260fa9e7
kenfelix <Kmrapper.kf@gmail.com>
2023-03-16 14:56:01 +0100

    manage state

--- app/Reducer.tsx
+++ app/Reducer.tsx
@@ -1,1 +1,26 @@
-import { useReducer } from "react"
+export const INITIAL_STATE = {
+    open: true,
+    infoSubmenu: true,
+    linkSubmenu: true,
+}
+
+const animationReducer = (state, action) => {
+    switch (action.type) {
+        case "OPEN_SIDENAV":
+            return {
+                ...state,
+                open: true,
+            };
+        case "OPEN_INFOSUBMENU":
+            return {
+                ...state,
+                infoSubmenu: true,
+            };
+        case "OPEN_LINKSUBMENU": {
+            return {
+                ...state,
+                linkSubmenu: true,
+            }
+        }
+    }
+}
