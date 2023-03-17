5c31688f1e73b85685c5582ca0394515b04a97b5
kenfelix <kmrapper.kf@gmail.com>
2023-03-17 10:58:10 +0100

    Responsiveness update

--- app/buy.tsx
+++ app/buy.tsx
@@ -5,11 +5,11 @@ import { useGlobalContext } from './Context/store'
 function Buy() {
     const {open} = useGlobalContext()
   return (
-    <><div className='flex flex-row gap-[25px] sm:gap-[90px] md:gap-[250px] mt-[30px]'>
-          <button className='bg-[#6C8726] px-[15px] rounded-[40px] py-[13px] sm:px-[20px] text-white font-semibold text-[14px] sm:text-[17px] lg:text-[20px]'>Purchase Token</button>
+    <><div className={`flex flex-row gap-[25px] sm:gap-[150px] md:gap-[250px] ${open && "md:gap-[100px]"} mt-[30px]`}>
+          <button className={`bg-[#6C8726] px-[15px] rounded-[40px] py-[13px] sm:px-[20px] text-white font-semibold text-[14px] sm:text-[17px] lg:text-[20px]`}>Purchase Token</button>
           <button className='bg-white  px-[15px] rounded-[40px] py-[13px] sm:px-[20px] text-[#6C8726] font-semibold text-[14px] sm:text-[17px] lg:text-[20px]'>Join Airdrop</button>
       </div>
-          <div className={`flex flex-col md:flex-row  items-center justify-between px-[30px] ${open && "md:flex-col gap-[60px]"} ${open && "lg:flex-row px-[150px]"} lg:px-[87px] py-[73px] w-full bg-[#140E3A] rounded-[27px]`}>
+          <div className={`flex flex-col gap-10 md:flex-row  items-center justify-evenly px-[30px] ${open && "md:flex-col gap-[60px]"} ${open && "lg:flex-row px-[150px]"} lg:px-[87px] py-[73px] w-full bg-[#140E3A] rounded-[27px]`}>
               <div className='flex flex-col gap-[65px]'>
                   <div className='leading-9'>
                       <h4 className='text-[#6C8726] font-bold text-[20px]'>PRESALE</h4>
